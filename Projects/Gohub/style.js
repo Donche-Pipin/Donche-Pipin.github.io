d423a25184a257ff9a4ccec319937f4fc0def0b0
Alex <donch.to.pipin@yandex.ru>
2019-10-30 17:32:16 +0300

    1

--- Projects/Gohub/style.js
+++ Projects/Gohub/style.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
     $(".menu-icon__item:nth-child(3)").toggleClass("menu-icon__rotate-left");
     $(".menu").slideToggle();
   });
+  $(".footer-list-container__item").click(function() {
+    $(this)
+      .children(".footer-list")
+      .slideToggle();
+  });
+  // $("body").click(function() {
+  //   $(".footer-list").slideUp();
+  // });
+  // $(".footer-list__title").click(function() {
+  //   $(this).css("border-bottom", "3px solid #d3d3d9");
+  // });
 });
