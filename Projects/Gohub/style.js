2522bbb3649c3888096cf268cb570d698566d1ad
Alex <donch.to.pipin@yandex.ru>
2019-10-31 15:07:57 +0300

    1

--- Projects/Gohub/style.js
+++ Projects/Gohub/style.js
@@ -15,7 +15,22 @@ $(document).ready(function() {
   // $("body").click(function() {
   //   $(".footer-list").slideUp();
   // });
-  // $(".footer-list__title").click(function() {
-  //   $(this).css("border-bottom", "3px solid #d3d3d9");
+  $(".footer-list__title").click(function() {
+    $(this).css("border-bottom", "3px solid #d3d3d9");
+  });
+  $(document).mouseup(function(e) {
+    // отслеживаем событие клика по веб-документу
+    let block = $(".footer-list"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
+    if (
+      !block.is(this) && // проверка условия если клик был не по нашему блоку
+      block.has(this).length === 0
+    ) {
+      // проверка условия если клик не по его дочерним элементам
+      block.hide(); // если условия выполняются - скрываем наш элемент
+      $(".footer-list__title").css("border-bottom", "");
+    }
+  });
+  // $(".open-form-button").click(function() {
+  //   $(".form").fadeIn();
   // });
 });
