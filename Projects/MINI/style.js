53bbb893a8d99b43cff5aca65fc45f6b11b8285b
Alex <donch.to.pipin@yandex.ru>
2019-11-11 15:26:26 +0300

    1

---
+++ Projects/MINI/style.js
@@ -0,0 +1,6 @@
+// $(document).ready(function() {
+//   $(".card").hover(function() {
+//     $(this).css("background-color", "#f0f0f0");
+//     $(".horizontal-line").css("background-color", "#cccccc");
+//   });
+// });
